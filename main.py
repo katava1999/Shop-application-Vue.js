d003aca046b76493364d329c320c370fbc1e1594
Nemanja Katava <katavanemanja99@gmail.com>
2021-06-16 15:38:18 +0200

    prikaz proizvoda

--- main.py
+++ main.py
@@ -183,4 +183,18 @@ def ukloni_korpu(korpa_id):
     return ""
 
 #TIP
-#NOVI TIP
+@app.route("/api/tip")
+def get_all_tip():
+    if session.get("korisnik") is not None:
+        cursor = mysql.get_db().cursor()
+        cursor.execute("SELECT * FROM tip")
+        tip = cursor.fetchall()
+        return flask.jsonify(tip)
+    return "", 401
+    
+@app.route("/api/tip/<int:tip_id>")
+def get_tip(tip_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM tip WHERE id=%s", (tip_id,))
+    tip = cursor.fetchone()
+    return flask.jsonify(tip)
--- static/app/components/login.js
+++ static/app/components/login.js
@@ -41,8 +41,6 @@ export default {
             
             });
             
-        }else {
-            this.neuspesanLogin=true
         }
     }
         
